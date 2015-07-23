0385fd4e1f747568be74010b217264b82ca55faf
Sacheendra Talluri <sacheendra.t@gmail.com>
2015-07-23 18:40:53 -0500

    better homepage example

--- get_test.go
+++ get_test.go
@@ -35,51 +35,21 @@ func TestStreamDocument(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	event := <-responseStream
-
-	if event == nil {
-		t.Errorf("Event not received")
-	}
-
-	close(responseStream)
-}
-
-func ExampleStreamDocument(t *testing.T) {
-	client, err := elastic.NewClient(elastic.SetURL("http://testuser:testpass@localhost:7999"), elastic.SetSniff(false))
-	if err != nil {
-		t.Error(err)
-	}
-
-	streamingClient, err := NewClient("http://localhost:7999", "testuser", "testpass", "testindex")
-	if err != nil {
-		t.Error(err)
-	}
-
-	tweet1 := `{"user": "olivere", "message": "Welcome to Golang and Elasticsearch."}`
-	_, err = client.Index().Index("testindex").Type("tweet").Id("1").BodyString(tweet1).Do()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Get document 1
-	initialResponse, responseStream, _, err := streamingClient.StreamDocument().Type("tweet").Id("1").Do()
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println(initialResponse)
-
-	_, err = client.Index().Index("testindex").Type("tweet").Id("1").BodyString(tweet1).Do()
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	for event := range responseStream {
 		if event == nil {
 			t.Errorf("Event not received")
 		}
-		fmt.Println(event)
 		break
 	}
+}
 
-	close(responseStream)
+func ExampleStreamDocument(t *testing.T) {
+	streamingClient, _ := NewClient("http://localhost:7999", "testuser", "testpass", "testindex")
+
+	initialResponse, responseStream, _, _ := streamingClient.StreamDocument().Type("tweet").Id("1").Do()
+	fmt.Println(initialResponse)
+
+	for event := range responseStream {
+		fmt.Println(event)
+	}
 }
