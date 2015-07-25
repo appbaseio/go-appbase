ec42cef9f485bcc01cc0f0c39d5ca484c69e8814
Sacheendra Talluri <sacheendra.t@gmail.com>
2015-07-24 23:35:05 -0500

    adds search test

--- search_test.go
+++ search_test.go
@@ -1,13 +1,13 @@
 package appbase
 
 import (
-	"fmt"
 	"testing"
+	"time"
 
 	"github.com/olivere/elastic"
 )
 
-func TestStreamDocument(t *testing.T) {
+func TestStreamSearch(t *testing.T) {
 	client, err := elastic.NewClient(elastic.SetURL("http://testuser:testpass@localhost:7999"), elastic.SetSniff(false))
 	if err != nil {
 		t.Error(err)
@@ -25,10 +25,18 @@ func TestStreamDocument(t *testing.T) {
 	}
 
 	// Get document 1
-	_, responseStream, _, err := streamingClient.StreamDocument().Type("tweet").Id("1").Do()
+	_, responseStream, _, err := streamingClient.StreamSearch().Type("tweet").Body(`
+		{
+			"query": {
+				"match_all": {}
+			}
+		}
+		`).Do()
 	if err != nil {
 		t.Error(err)
 	}
+
+	time.Sleep(time.Second * 2)
 
 	_, err = client.Index().Index("testindex").Type("tweet").Id("1").BodyString(tweet1).Do()
 	if err != nil {
@@ -47,16 +55,5 @@ func TestStreamDocument(t *testing.T) {
 			t.Error("event was not as expected: ", string(e))
 		}
 		break
-	}
-}
-
-func ExampleStreamDocument(t *testing.T) {
-	streamingClient, _ := NewClient("http://localhost:7999", "testuser", "testpass", "testindex")
-
-	initialResponse, responseStream, _, _ := streamingClient.StreamDocument().Type("tweet").Id("1").Do()
-	fmt.Println(initialResponse)
-
-	for event := range responseStream {
-		fmt.Println(event)
 	}
 }
